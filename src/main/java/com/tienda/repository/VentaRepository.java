46bb9715ee4b6de0335f838ce3de3a4c8da9917d
Jeff2476 <131559715+Jeff2476@users.noreply.github.com>
2026-08-14 09:43:12 -0600

    Semana 12 - Repositorio real de ventas

---
+++ src/main/java/com/tienda/repository/VentaRepository.java
@@ -0,0 +1,7 @@
+package com.tienda.repository;
+
+import com.tienda.domain.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VentaRepository extends JpaRepository<Venta, Integer>{
+}
