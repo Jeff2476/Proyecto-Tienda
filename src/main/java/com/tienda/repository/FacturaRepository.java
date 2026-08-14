d7fa387791b85330994a12e92d2099d403ae71ce
Jeff2476 <131559715+Jeff2476@users.noreply.github.com>
2026-08-14 09:43:07 -0600

    Semana 12 - Repositorio real de facturas

---
+++ src/main/java/com/tienda/repository/FacturaRepository.java
@@ -0,0 +1,16 @@
+package com.tienda.repository;
+
+import com.tienda.domain.Factura;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface FacturaRepository extends JpaRepository<Factura, Integer>{
+    @Query("SELECT f FROM Factura f " +
+           "LEFT JOIN FETCH f.usuario u " +
+           "LEFT JOIN FETCH f.ventas v " +
+           "LEFT JOIN FETCH v.producto p " +
+           "WHERE f.idFactura = :idFactura")
+    Optional<Factura> findByIdFacturaConDetalle(@Param("idFactura") Integer idFactura);
+}
